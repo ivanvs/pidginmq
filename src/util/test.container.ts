19781fca275bb53ed1a98caeb1334d9495ba0708
ivanvs <robince885@gmail.com>
2024-08-06 01:40:43 +0200

    Fix tests

--- src/util/test.container.ts
+++ src/util/test.container.ts
@@ -3,7 +3,7 @@ import { PostgreSqlContainer } from '@testcontainers/postgresql';
 export const getTestContainer = () => {
   return new PostgreSqlContainer().withCopyFilesToContainer([
     {
-      source: 'migration/01_create_initial_schema.sql',
+      source: 'raw_sql/001_create_initial_schema.sql',
       target: '/docker-entrypoint-initdb.d/01-schema.sql',
     },
   ]);
